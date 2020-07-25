550f48f6ef195f01a365ac1abe66690dc9e72a41
Denis Nagy <denysnagy@gmail.com>
2020-07-25 10:21:52 +0100

    video convertion and upload

--- includes/classes/VideoProcessor.php
+++ includes/classes/VideoProcessor.php
@@ -3,9 +3,13 @@ class VideoProcessor {
     private $con;
     private $sizeLimit = 500000000;
     private $allowedTypes = array("mp4", "flv", "webm", "mkv", "vob", "ogv", "ogg", "avi", "wmv", "mov", "mpeg", "mpg");
+    private $ffmpegPath; // = "ffmpeg/bin/ffmpeg"; -> linux environment
+    private $ffprobePath; // = "ffmpeg/bin/ffmpeg"; -> linux environment
 
     public function __construct($con) {
         $this->con = $con;
+        $this->ffmpegPath = realpath("ffmpeg/bin/ffmpeg.exe"); // for windows environments
+        $this->ffprobePath = realpath("ffmpeg/bin/ffprobe.exe"); // for windows environments
     }
 
     public function upload($videoUploadData) {
@@ -25,7 +29,28 @@ class VideoProcessor {
         }
 
         if(move_uploaded_file($videoData["tmp_name"], $tempFilePath)) {
-            echo "file moved succesfuly";
+            
+            $finalFilePath = $targetDir . uniqid() . ".mp4";
+
+            if(!$this->insertVideoData($videoUploadData, $finalFilePath)) {
+                echo "Insert query failed";
+                return false;
+            }
+
+            if(!$this-> convertVideoToMp4($tempFilePath, $finalFilePath)) {
+                echo "upload failed";
+                return false;
+            }
+
+            if(!$this-> deleteFile($tempFilePath)) {
+                echo "upload failed";
+                return false;
+            }
+
+            if(!$this-> generateThumbnails($finalFilePath)) {
+                echo "upload failed - could not generate thumbnail";
+                return false;
+            }
         }
     }
 
@@ -48,6 +73,7 @@ class VideoProcessor {
         return true;
     }
 
+    // Check for the size of input file
     private function isValidSize($data) {
         return $data["size"] <= $this->sizeLimit;
     }
@@ -61,6 +87,64 @@ class VideoProcessor {
     private function hasError($data) {
         return $data["error"] != 0;
     }
+
+    // Inset video data into database
+    private function insertVideoData($uploadData, $filePath) {
+        
+        $query = $this->con->prepare("INSERT INTO videos(title, uploadedBy, description, privacy, category, filePath)
+                                        VALUES(:title, :uploadedBy, :description, :privacy, :category, :filePath)");
+
+        $query->bindParam(":title", $uploadData->title);
+        $query->bindParam(":uploadedBy", $uploadData->uploadedBy);
+        $query->bindParam(":description", $uploadData->description);
+        $query->bindParam(":privacy", $uploadData->privacy);
+        $query->bindParam(":category", $uploadData->category);
+        $query->bindParam(":filePath", $filePath);
+
+        return $query->execute();
+    }
+
+    // Convert video format to mp4
+    public function convertVideoToMp4($tempFilePath, $finalFilePath) {
+        $cmd = "$this->ffmpegPath -i $tempFilePath $finalFilePath 2>&1";
+
+        $outputLog = array();
+        exec($cmd, $outputLog, $returnCode);
+
+        if($returnCode != 0) {
+            foreach($outputLog as $line) {
+                echo $line . "<br>";
+            }
+            return false;
+        }
+        return true;
+    }
+
+    // Delete the original file 
+    private function deleteFile($filePath) {
+        if(!unlink($filePath)) {
+            echo "could not delete\n";
+            return false;
+        }
+
+        return true;
+    }
+
+    // Generate thumbnails images for videos
+    public function generateThumbnails($filePath) {
+        $thumbNailSize = "210x118";
+        $numThumbnails = 3;
+        $pathToThumbnail = "uploads/videos/thumbnails";
+
+        $duration = $this->getVideoDuration($filePath);
+
+        echo "duration: $duration";
+    }
+
+    private function getVideoDuration($filePath) {
+        return shell_exec("$this->ffprobePath -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $filePath");
+    }
+    
 }
 
 ?>
