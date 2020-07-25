1fe3160a66fd0c8eb3f7996acc513487cf985cc1
Denis Nagy <denysnagy@gmail.com>
2020-07-25 14:10:49 +0100

    create the signin page

---
+++ signin.php
@@ -0,0 +1,33 @@
+<?php require_once("includes/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sign In - One Stage</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="signInContainer">
+        <div class="column">
+
+            <div class=header>
+
+            </div>
+
+            <div class="loginForm">
+            
+            </div>
+
+            <a class="signInMessage" href="signIn.php">
+                Already have an account? sign in here!
+            </a>
+
+        </div>
+    </div>
+
+</body>
+</html>
